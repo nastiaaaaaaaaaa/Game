f9d86e378933674ce0dcaa491d97fb9bf1f23ade
nastiaaaaaaaaaa <116444052+nastiaaaaaaaaaa@users.noreply.github.com>
2024-11-16 17:16:37 +0200

    Update my.py

--- nastia_labirunt/my.py
+++ nastia_labirunt/my.py
@@ -1,43 +1,81 @@
-from pygame import*
+from pygame import *
 
+# Define the GameSprite class
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65,65))
+        self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
+    
     def reset(self):
-        window.blit(self.image,(self.rect.x)(self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):  
+    direction = 'left'
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = 'right'
+        if self.rect.x >= win_width - 85:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        else:
+            self.rect.x <= self.speed
+
+# Game setup
 win_width = 700
 win_height = 500
-
 window = display.set_mode((win_width, win_height))
 display.set_caption('Maze')
 background = transform.scale(image.load('background.jpg'), (win_width, win_height))
 
-player = GameSprite('hero.png', 5, win_height - 80, 4 )
-monster = GameSprite('cyborg.png', win_width - 80, 200, 0)
+# Game characters
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
+# Game variables
 game = True
+finish = False
 clock = time.Clock()
 FPS = 60
 
+# Music setup
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 
+# Game loop
 while game:
     for e in event.get():
         if e.type == QUIT:
-            game = False 
+            game = False
+    if finish != True:
+        window.blit(background,(0,0))
+        player.update()
+        monster.update()
 
-window.blit(background,(0,0))
-player.reset()
-monster.reset()
-display.update()
-clock.tick(FPS)
+    # Draw the background and sprites
+    window.blit(background, (0, 0))
+    player.reset()
+    monster.reset()
+    final.reset()
+
+    # Update display and control the frame rate
+    display.update()
+    clock.tick(FPS)
